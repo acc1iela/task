287546c3a4b459a4fb5464d1d142d68221b75bbb
Accio26 <acc1iela@gmail.com>
2020-06-15 23:16:02 +0900

    tab部分完成

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function () {
     $('.is-active').removeClass('is-active');
     $(this).addClass('is-active');
     $('.is-show').removeClass('is-show');
-    var index = $(this).index();
+    const index = $(this).index();
     $('.p-tabbedPanel__contents').eq(index).addClass('is-show');
   });
 });
