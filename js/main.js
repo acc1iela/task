655d86dc2c64ebe055088fe955dc6d36b06992b5
Accio26 <acc1iela@gmail.com>
2020-07-01 02:26:22 +0900

    形完成

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 $(function () {
+  // タブ切り替え
   $('.js-tabbed').click(function () {
     $('.is-active').removeClass('is-active');
     $(this).addClass('is-active');
     $('.is-show').removeClass('is-show');
     const index = $(this).index();
     $('.p-tabbedPanel__contents').eq(index).addClass('is-show');
+  });
+  // アコーディオン
+  $('.p-accordion__inner:not(:first)').css('display', 'none');
+  $('.js-toggle').click(function () {
+    $('.js-toggle').not(this).removeClass('open').next().slideUp();
+    $(this).toggleClass('open').next().slideToggle();
   });
 });
